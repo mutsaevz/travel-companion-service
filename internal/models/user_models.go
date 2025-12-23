73a0875b7682281ef14ba2420a856e23453a3f57
Ali Umarov <aliumarov90@mail.ru>
2025-12-23 20:50:44 +0300

    дописал горм теги для пользователя и поездки

--- internal/models/user_models.go
+++ internal/models/user_models.go
@@ -3,9 +3,9 @@ package models
 type User struct {
 	Base
 
-	Name    string `json:"name"`
-	Phone   string `json:"phone"`
-	Balance int    `json:"balance"`
+	Name    string `json:"name" gorm:"type:varchar(255);not null"`
+	Phone   string `json:"phone" gorm:"type:varchar(20);not null;unique;index"`
+	Balance int    `json:"balance" gorm:"not null;default:0;check:balance >= 0"`
 }
 
 type UserCreateRequest struct {
